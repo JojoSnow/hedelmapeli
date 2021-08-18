3a1b5c4179f230481094bfa6bccf132592e148d1
JojoSnow <milzwow94@gmail.com>
2021-08-18 14:47:08 +0300

    end of the day

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let playMoney = 50;
-let betMoneyArray = [1, 2, 3];
-let winMoney = [2, 4, 6, 8];
-let randomImgArray = ['img/apple.png', 'img/cherry.png', 'img/pear.png', 'img/watermelon.png'];
+let chosenBet = 1;
+const betMoneyArray = [1, 2, 3];
+const winMoneyArray = [2, 4, 6, 8];
+const randomImgArray = ['/img/apple.png', '/img/cherry.png', '/img/pear.png', '/img/watermelon.png'];
 
 let betEvent = document.querySelectorAll('.bet-item');
 let btnClick = document.getElementById('button');
 
 betEvent.forEach(bet => bet.addEventListener('mouseup', selectBet));
-btnClick.addEventListener('click', playGame);
+btnClick.addEventListener('click', startGame);
 
 function selectBet(event) {
     let betTarget = event.target;
     let target = '';
 
-    if(betTarget === document.getElementById('one-bet')) {
+    if (betTarget === document.getElementById('one-bet')) {
         target = betTarget.getAttribute('id');
     } else if (betTarget === document.getElementById('two-bet')) {
         target = betTarget.getAttribute('id');
@@ -26,12 +27,15 @@ function selectBet(event) {
     switch(target) {
         case 'one-bet':
             document.getElementById('bet-money').innerHTML = betMoneyArray[0];
+            chosenBet = 1;
             break;
         case 'two-bet':
             document.getElementById('bet-money').innerHTML = betMoneyArray[1];
+            chosenBet = 2;
             break;
         case 'three-bet':
             document.getElementById('bet-money').innerHTML = betMoneyArray[2];
+            chosenBet = 3;
             break;
         default:
             console.log('innerHTML bet error');
@@ -39,12 +43,74 @@ function selectBet(event) {
     }
 }
 
-function playGame() {
-    let randomNum = Math.floor(Math.random() * randomImgArray.length);
-    let imgOne = document.getElementById('slot-one');
-    let imgTwo = document.getElementById('slot-two');
-    let imgThree = document.getElementById('slot-three');
-    let imgFour = document.getElementById('slot-four');
+function startGame() {
+    randomizeSlots();
+    countMoney();
+}
+
+function randomizeSlots() {
     
-    imgOne.scr = randomImgArray[randomNum];
+    const slots = document.querySelectorAll('.roulette-list-item');
+
+    slots.forEach(slot => slot.firstElementChild.remove());
+
+    for (let i = 0; slots.length > i; i++) {
+        let randomNum = Math.floor(Math.random() * randomImgArray.length);
+        let img = document.createElement('img');
+        img.src = randomImgArray[randomNum];
+        img.alt = 'random-image';
+        slots[i].appendChild(img);
+    }
+}
+
+function countMoney() {
+
+    let slots = document.querySelectorAll('.roulette-list-item img');
+    let apple = 0;
+    let cherry = 0;
+    let pear = 0;
+    let watermelon = 0;
+
+    for (let i = 0; slots.length > i; i++) {
+        let slotSrc = slots[i].getAttribute('src');
+        switch (slotSrc) {
+            case '/img/apple.png':
+                apple++;
+                break;
+            case '/img/cherry.png':
+                cherry++;
+                break;
+            case '/img/pear.png':
+                pear++;
+                break;
+            case '/img/watermelon.png':
+                watermelon++;
+                break;
+            default:
+                console.log('slots win/lose error');
+        }
+    }
+    const winOrLose = document.getElementById('win-or-lose-text');
+    let money = document.getElementById('money');
+    if (apple == 4) {
+        playMoney = playMoney + (winMoneyArray[0] * chosenBet / 2);
+        money.innerText = playMoney;
+        winOrLose.innerText = 'Voitto!';
+    } else if (cherry == 4) {
+        playMoney = playMoney + (winMoneyArray[1] * chosenBet / 2);
+        money.innerText = playMoney;
+        winOrLose.innerText = 'Voitto!';
+    } else if (pear == 4) {
+        playMoney = playMoney + (winMoneyArray[2] * chosenBet / 2);
+        money.innerText = playMoney;
+        winOrLose.innerText = 'Voitto!';
+    } else if (watermelon == 4) {
+        playMoney = playMoney + (winMoneyArray[3] * chosenBet / 2);
+        money.innerText = playMoney;
+        winOrLose.innerText = 'Voitto!';
+    } else {
+        playMoney = playMoney - chosenBet;
+        money.innerText = playMoney;
+        winOrLose.innerText = 'Ei voittoa';
+    }
 }
